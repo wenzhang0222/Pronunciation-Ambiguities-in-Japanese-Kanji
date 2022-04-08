4643641b67f9939e081a25c9d6b79029e91d63f5
Wen Zhang <79327852+wenzhang0222@users.noreply.github.com>
2022-04-08 09:57:26 -0400

    Update week9_dataclasses.py

--- week9_dataclasses.py
+++ week9_dataclasses.py
@@ -15,14 +15,10 @@ class Entry:
 def main() -> None:
     table = collections.defaultdict(list)
     with open("jiten_revised.tsv", "r") as source:
-        reader = csv.reader(source)
-        next(reader, None)
-        for line in source:
-            homograph = line[0]
-            tp = line[2]
-            read = line[1]
-            table[homograph].append(Entry(tp, read))
-        pprint.pprint(table)
+        reader = csv.reader(source, delimiter = "\t")
+        for homograph, reading, homography in reader:
+            table[homograph].append(Entry(homography, reading))
+    pprint.pprint(table)
 
 
 if __name__ == "__main__":
