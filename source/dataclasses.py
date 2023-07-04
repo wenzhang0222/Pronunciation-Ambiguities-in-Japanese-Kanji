ef4dc7e47dde8c3be69395f2e04e1f3a5f1d1d37
Wen Zhang <zhangwen6422happy@gmail.com>
2023-07-04 12:04:27 -0400

    Modeling code

---
+++ source/dataclasses.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import csv
+import collections
+import dataclasses
+import pprint
+
+
+@dataclasses.dataclass
+class Entry:
+    homography: str   # "Type" in the TSV file.
+    reading: str      # "Reading" in the TSV file.
+
+
+def main() -> None:
+    table = collections.defaultdict(list)
+    with open("jiten_revised.tsv", "r") as source:
+        reader = csv.reader(source, delimiter = "\t")
+        for homograph, reading, homography in reader:
+            table[homograph].append(Entry(homography, reading))
+    pprint.pprint(table)
+
+
+if __name__ == "__main__":
+    main()
